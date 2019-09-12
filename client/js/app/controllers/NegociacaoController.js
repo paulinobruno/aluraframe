00839c017752734c24e8d9bc03c8b54c6335a051
Bruno Paulino <paulino.bruno@gmail.com>
2019-09-12 09:14:22 -0300

    :sparkles: add: class 04

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,23 +4,31 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event) {
     event.preventDefault();
 
-    let data = new Date(...
-      this._inputData.value
-        .split('-')
-        .map((elem, index) => elem - index % 2)
-    );
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();
 
-    let negociacao = new Negociacao(
-      data,
+    console.log(this._listaNegociacoes.negociacoes);
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
+  }
 
-    console.log(negociacao);
+  _limpaFormulario() {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0
+
+    this._inputData.focus();
   }
 }
