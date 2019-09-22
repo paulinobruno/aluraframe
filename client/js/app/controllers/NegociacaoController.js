bccba33042791a14914777f8971b962588a6d678
Bruno Paulino <paulino.bruno@gmail.com>
2019-09-22 11:40:06 -0300

    :sparkles: add: class 04, activity 03
    Impedir a importação da mesma negociação.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -52,9 +52,15 @@ class NegociacaoController {
     let service = new NegociacaoService();
 
     service.obterNegociacoes()
+      .then(negociacoes =>
+        negociacoes.filter(negociacao =>
+          !this._listaNegociacoes.negociacoes.some(negociacaoExistente =>
+            JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)
+          )
+        )
+      )
       .then(negociacoes => {
-        negociacoes
-          .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+        negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
         this._mensagem.texto = 'Negociações importadas com sucesso';
       })
       .catch(erro => this._mensagem.texto = erro);
