9eec0700e97f5f3d6f4b284ae9a39c50224bf688
Bruno Paulino <paulino.bruno@gmail.com>
2019-09-22 12:22:52 -0300

    :sparkles: add: class 05
    Fetch API

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,41 +1,24 @@
 class HttpService {
   get(url) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-
-      xhr.open('GET', url);
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            console.log(xhr.responseText);
-            reject(xhr.responseText);
-          }
-        }
-      }
-
-      xhr.send();
-    });
+    return fetch(url)
+      .then(this._handleErrors)
+      .then(res => res.json());
   }
 
   post(url, dado) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
+    return fetch(url, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'post',
+      body: JSON.stringify(dado)
+    })
+      .then(this._handleErrors);
+  }
 
-      xhr.open("POST", url, true);
-      xhr.setRequestHeader("Content-type", "application/json");
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            reject(xhr.responseText);
-          }
-        }
-      };
+  _handleErrors(res) {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
 
-      xhr.send(JSON.stringify(dado));
-    });
+    return res;
   }
 }
