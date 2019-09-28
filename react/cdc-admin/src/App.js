f7886fb30bd89bfe2f8a1b979b6a36817d79a952
Bruno Paulino <paulino.bruno@gmail.com>
2019-09-28 15:54:06 -0300

    :sparkles: add: class 05, act 01
    Refatorando em componentes com responsabilidades mais específicas:
    - Formulário de Autor;
    - Listagem de Autores.

--- react/cdc-admin/src/App.js
+++ react/cdc-admin/src/App.js
@@ -1,60 +1,10 @@
 import React, { Component } from 'react';
-import Input from './components/Input';
-import Button from './components/Button';
+import { Lista, Formulario } from './Autor';
 import './css/pure-min.css';
 import './css/side-menu.css';
 
-const _handleHttpErrors = res => {
-  if (!res.ok) {
-    throw new Error(res.statusText);
-  }
-
-  return res;
-}
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      lista: [],
-      nome: '',
-      email: '',
-      senha: ''
-    };
-
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setValue = this.setValue.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:8080/api/autores')
-      .then(_handleHttpErrors)
-      .then(response => response.json())
-      .then(lista => this.setState({ lista }));
-  }
-
-  enviaForm(event) {
-    event.preventDefault();
-
-    const { nome, email, senha } = this.state;
-
-    fetch('http://localhost:8080/api/autores', {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'post',
-      body: JSON.stringify({ nome, email, senha })
-    })
-      .then(_handleHttpErrors)
-      .then(response => response.json())
-      .then(lista => this.setState({ lista }))
-      .catch(x => console.error('erro', x));
-  }
-
-  setValue(event) {
-    const { id: fieldName, value } = event.target;
-    this.setState({ [fieldName]: value });
-  }
-
   render() {
     return (
       <div id="layout">
@@ -80,35 +30,8 @@ class App extends Component {
           </div>
 
           <div className="content" id="content">
-
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
-                <Input label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setValue} />
-                <Input label="E-mail" id="email" type="email" name="email" value={this.state.email} onChange={this.setValue} />
-                <Input label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setValue} />
-                <Button label="Gravar" />
-              </form>
-            </div>
-
-            <div>
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>E-mail</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.lista.map(item =>
-                    <tr key={item.id}>
-                      <td>{item.nome}</td>
-                      <td>{item.email}</td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-
+            <Formulario />
+            <Lista />
           </div>
         </div>
       </div>
---
+++ react/cdc-admin/src/Autor.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import Input from './components/Input';
+import Button from './components/Button';
+
+const _handleHttpErrors = res => {
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+
+  return res;
+}
+
+export class Lista extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lista: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/api/autores')
+      .then(_handleHttpErrors)
+      .then(response => response.json())
+      .then(lista => this.setState({ lista }));
+  }
+
+  render() {
+    return (
+      <div>
+        <table className="pure-table">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>E-mail</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.lista.map(item =>
+              <tr key={item.id}>
+                <td>{item.nome}</td>
+                <td>{item.email}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export class Formulario extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nome: '',
+      email: '',
+      senha: ''
+    };
+
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setValue = this.setValue.bind(this);
+  }
+
+  enviaForm(event) {
+    event.preventDefault();
+
+    const { nome, email, senha } = this.state;
+
+    fetch('http://localhost:8080/api/autores', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'post',
+      body: JSON.stringify({ nome, email, senha })
+    })
+      .then(_handleHttpErrors)
+      .then(response => response.json())
+      .then(lista => this.setState({ lista }))
+      .catch(x => console.error('erro', x));
+  }
+
+  setValue(event) {
+    const { id: fieldName, value } = event.target;
+    this.setState({ [fieldName]: value });
+  }
+
+  render() {
+    return (
+      <div className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
+          <Input label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setValue} />
+          <Input label="E-mail" id="email" type="email" name="email" value={this.state.email} onChange={this.setValue} />
+          <Input label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setValue} />
+          <Button label="Gravar" />
+        </form>
+      </div>
+    );
+  }
+}
