fb2b9f35f860669436cc2d9ee75df2df3b8dd443
Bruno Paulino <paulino.bruno@gmail.com>
2019-09-27 22:42:12 -0300

    :sparkles: add: class 06, act 10
    Trabalhando com variáveis de ambiente.

--- webpack/client/webpack.config.js
+++ webpack/client/webpack.config.js
@@ -27,7 +27,10 @@ const plugins = [
   }),
 ];
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
 if (process.env.NODE_ENV === 'production') {
+  SERVICE_URL = JSON.stringify('http://api.paulinobruno.com');
+
   plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
   plugins.push(new BabiliPlugin());
   plugins.push(new OptimizeCssAssetPlugin({
@@ -38,6 +41,8 @@ if (process.env.NODE_ENV === 'production') {
     canPrint: true
   }));
 }
+
+plugins.push(new webpack.DefinePlugin({ SERVICE_URL }));
 
 module.exports = {
   entry: {
