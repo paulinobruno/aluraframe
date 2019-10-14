aba913f51351523243218355c71f151de560094a
Bruno Paulino <paulino.bruno@gmail.com>
2019-10-13 22:08:19 -0300

    :sparkles: add: class 03, act 02
    POST de autenticação.

--- react-parte-2/instalura/src/Login.js
+++ react-parte-2/instalura/src/Login.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-  <div className="login-box">
-    <h1 className="header-logo">Instalura</h1>
-    <form>
-      <input type="text" />
-      <input type="password" />
-      <input type="submit" value="login" />
-    </form>
-  </div>
-);
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: ''
+    };
+  }
+
+  envia(event) {
+    event.preventDefault();
+
+    fetch('http://localhost:8080/api/public/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        login: this.username.value,
+        senha: this.password.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(resp => {
+        if (resp.ok) {
+          return resp.text();
+        } else {
+          throw new Error('Não foi possível autenticar')
+        }
+      })
+      .then(token => console.log(token))
+      .catch(err => this.setState({ msg: err.message }));
+  }
+
+  render() {
+    return (
+      <div className="login-box">
+        <h1 className="header-logo">Instalura</h1>
+        {!!this.state.msg && <span>{this.state.msg}</span>}
+        <form onSubmit={this.envia.bind(this)}>
+          <input type="text" ref={input => this.username = input}/>
+          <input type="password" ref={input => this.password = input}/>
+          <input type="submit" value="login" />
+        </form>
+      </div>
+    );
+  }
+}
