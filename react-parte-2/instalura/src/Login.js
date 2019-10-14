85c82bb368fdf93999162518d1cff8aa2da79dbe
Bruno Paulino <paulino.bruno@gmail.com>
2019-10-13 21:48:38 -0300

    :sparkles: add: class 03, act 01
    Tela de login.

---
+++ react-parte-2/instalura/src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div className="login-box">
+    <h1 className="header-logo">Instalura</h1>
+    <form>
+      <input type="text" />
+      <input type="password" />
+      <input type="submit" value="login" />
+    </form>
+  </div>
+);
--- react-parte-2/instalura/src/index.js
+++ react-parte-2/instalura/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './Login';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './css/normalize.min.css';
 import './css/timeline.css';
+import './css/login.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter history={createBrowserHistory}>
+    <Route exact path="/" component={Login} />
+    <Route path="/timeline" component={App} />
+  </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
