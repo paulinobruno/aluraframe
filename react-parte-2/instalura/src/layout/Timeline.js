6aae716b0628d45ddc164c36e825f3e5cf57e378
Bruno Paulino <paulino.bruno@gmail.com>
2019-10-20 19:34:23 -0300

    :sparkles: add: class 04, act 03
    Forçando atualização do componente em função de novos parâmetros

--- react-parte-2/instalura/src/layout/Timeline.js
+++ react-parte-2/instalura/src/layout/Timeline.js
@@ -1,38 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Photo from '../componentes/Photo';
 import { store } from '../security/TokenStore';
 
-export default class Timeline extends Component {
-  constructor() {
-    super();
-    this.state = {
-      photos: []
-    };
-  }
+export default ({ user }) => {
+  const [photos, setPhotos] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     const authParam = `X-AUTH-TOKEN=${store.getValue()}`;
     let timelineUrl = 'http://localhost:8080/api/fotos';
 
-    const { user } = this.props;
     if (user !== undefined) {
       timelineUrl = `http://localhost:8080/api/public/fotos/${user}`;
     }
 
     fetch(`${timelineUrl}?${authParam}`)
       .then(resp => resp.json())
-      .then(photos => this.setState({ photos }));
-  }
+      .then(setPhotos);
+  }, [user]);
 
-  render() {
-    return (
-      <div className="fotos container">
-        {
-          this.state.photos.map(p =>
-            (<Photo key={p.id} data={p}></Photo>)
-          )
-        }
-      </div>
-    );
-  }
+  return (
+    <div className="fotos container">
+      {
+        photos.map(p =>
+          (<Photo key={p.id} data={p}></Photo>)
+        )
+      }
+    </div>
+  );
 }
