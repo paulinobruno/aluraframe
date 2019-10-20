cc202e51966fbfd307e96e494083b5cd09af14ea
Bruno Paulino <paulino.bruno@gmail.com>
2019-10-20 19:14:32 -0300

    :construction: add: class 04, act 02
    Link para perfil público.

--- react-parte-2/instalura/src/componentes/Photo/PhotoInfo.js
+++ react-parte-2/instalura/src/componentes/Photo/PhotoInfo.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import UserLink from '../UserLink';
 
 export default ({ id, loginUsuario, comentario, likers, comentarios }) => (
   <div className="foto-info">
     <div className="foto-info-likes">
       {
-        likers.map(liker => {
-          const hash = `#${id}#${liker.login}`;
-
-          return <a key={hash} href={hash}>{liker.login},</a>;
-        })
+        likers.map(liker =>
+          <UserLink toUser={liker.login} key={`#${id}#${liker.login}`} />
+        )
       }
       <span>{likers.length > 1 ? 'curtiram' : 'curtiu'}</span>
     </div>
 
     <p className="foto-info-legenda">
-      <a className="foto-info-autor" href="#photoInfo#3">{loginUsuario}</a>
+      <UserLink toUser={loginUsuario} className="foto-info-autor" />
       {comentario}
     </p>
 
@@ -22,7 +21,7 @@ export default ({ id, loginUsuario, comentario, likers, comentarios }) => (
       {
         comentarios.map(comentario =>
           <li className="comentario" key={`foto_${id}_comentario_${comentario.id}`}>
-            <a className="foto-info-autor" href="#photoInfo#4">{comentario.login}</a>
+            <UserLink toUser={comentario.login} className="foto-info-autor" />
             {comentario.texto}
           </li>
         )
