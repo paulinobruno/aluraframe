a22ea5dfed581d2acf0be739ffa2452d3c5c3f86
Bruno Paulino <paulino.bruno@gmail.com>
2019-10-30 09:09:42 -0300

    :sparkles: add: class 03, act 01
    Immutablejs parte 1

--- react-parte-3/instalura/src/reducers/timeline.js
+++ react-parte-3/instalura/src/reducers/timeline.js
@@ -1,14 +1,20 @@
-export const timeline = (state = [], action) => {
+import { List } from 'immutable';
+
+export const timeline = (state = new List(), action) => {
   if (action.type === 'LISTAGEM') {
-    return action.fotos;
+    return new List(action.fotos);
   }
 
   if (action.type === 'COMENTARIO') {
     const { fotoId, novoComentario } = action;
-    const fotoAchada = state.find(foto => foto.id === fotoId);
-    fotoAchada.comentarios.push(novoComentario);
 
-    return state;
+    const fotoAchada = state.find(foto => foto.id === fotoId);
+    const novosComentarios = [...fotoAchada.comentarios, novoComentario];
+    const novaFoto = { ...fotoAchada, comentarios: novosComentarios };
+    const currIndex = state.findIndex(foto => foto.id === fotoId);
+
+
+    return state.set(currIndex, novaFoto);
   }
 
   if (action.type === 'LIKE') {
